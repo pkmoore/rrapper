266ab56c17699ac160a2d9c4ac1da6496f4c4a23
Preston Moore <prestonkmoore@gmail.com>
2017-11-20 09:46:13 -0800

    Make attach to and continue processes

--- rreplay.py
+++ rreplay.py
@@ -3,12 +3,30 @@ from __future__ import print_function
 import os
 import sys
 import subprocess
+import re
 import ConfigParser
+import time
+from syscallreplay import syscallreplay
+
 
 if __name__ == '__main__':
     cfg  = ConfigParser.SafeConfigParser()
     cfg.read(sys.argv[1])
     #os.environ['RR_LOG'] = 'ReplaySession'
+    f = open('proc.out', 'w', 0)
     for i in cfg.sections():
-        subprocess.call('rr replay -a -n ' + cfg.get(i, 'event'), shell=True)
-
+        command = ['rr', 'replay', '-a', '-n', cfg.get(i, 'event')]
+        proc = subprocess.Popen(command, stdout=f, stderr=f)
+    time.sleep(3)
+    f.close()
+    f = open('proc.out', 'r')
+    lines = f.readlines()
+    for i in lines:
+        print(i.strip())
+    lines = [x.strip().split(' ') for x in lines if re.match('EVENT: [0-9]+ PID: [0-9]+', x)]
+    procs = [{'event': x[1], 'pid': x[3]} for x in lines]
+    print(procs)
+    for i in procs:
+        syscallreplay.attach(int(i['pid']))
+        syscallreplay.sigcont(int(i['pid']))
+        syscallreplay.sigcont(int(i['pid']))
