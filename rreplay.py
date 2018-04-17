a386a180e524fb78c7827f563f188d6b9d3ec6fc
Preston Moore <prestonkmoore@gmail.com>
2018-04-17 11:50:51 -0700

    Accept new format from rr, kill other_procs

--- rreplay.py
+++ rreplay.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 
 import os
 import os.path
+import signal
 import sys
 import subprocess
 import re
@@ -20,13 +21,15 @@ if __name__ == '__main__':
     subjects = []
     for i in cfg.sections():
         subjects.append({'event': cfg.get(i, 'event'),
-                         'pid': cfg.get(i, 'pid'),
+                         'rec_pid': cfg.get(i, 'pid'),
                          'trace_file': cfg.get(i, 'trace_file'),
                          'trace_start': cfg.get(i, 'trace_start'),
-                         'trace_end': cfg.get(i, 'trace_end')})
+                         'trace_end': cfg.get(i, 'trace_end'),
+                         'injected_state': str(cfg.get(i, 'event')) + '_state.json',
+                         'other_procs': [],})
     events_str = ''
     for i in subjects:
-        events_str += i['pid'] + ':' + i['event'] + ','
+        events_str += i['rec_pid'] + ':' + i['event'] + ','
     command = ['rr', 'replay', '-a', '-n', events_str]
     f = open('proc.out', 'w')
     proc = subprocess.Popen(command, stdout=f, stderr=f)
@@ -39,28 +42,48 @@ if __name__ == '__main__':
     subject_index = 0
     handles = []
     # A message on the pipe looks like:
-    # EVENT: <event number> PID: <pid>\n
+    # INJECT: EVENT: <event number> PID: <pid> REC_PID: <rec_pid>\n
+    # or
+    # DONT_INJECT: EVENT: <event number> PID: <pid> REC_PID: <rec_pid>\n
+    subjects_injected = 0
     while True:
         buf += f.read(1)
         if buf[-1] == '\n':
-            s = subjects[subject_index]
-            # The pid we want is in index 3 of the split message list
-            s['pid'] = buf.split(' ')[3]
-            handles.append({'event': i['event'],
-                            'handle': subprocess.Popen(['python',
-                                                        './inject.py',
-                                                        s['pid'],
-                                                        s['event'],
-                                                        s['trace_file'],
-                                                        s['trace_start'],
-                                                        s['trace_end'],
-                                                        str(s['event']) +
-                                                        '_state.json'])})
-            subject_index += 1
-        if subject_index == len(subjects):
-            break
+            print(buf)
+            parts = buf.split(' ')
+            inject = parts[0].strip()[:-1]
+            event = parts[2]
+            pid = parts[4]
+            rec_pid = parts[6].strip()
+
+            operating = [x for x in subjects if x['event'] == event]
+            # HACK HACK HACK: we only support spinning off once per event now
+            s = operating[0]
+            if inject == 'INJECT':
+                s['pid'] = pid
+                s['handle'] =  subprocess.Popen(['python',
+                                                 './inject.py',
+                                                 s['pid'],
+                                                 s['event'],
+                                                 s['trace_file'],
+                                                 s['trace_start'],
+                                                 s['trace_end'],
+                                                 s['injected_state']])
+                subjects_injected += 1
+            elif inject == 'DONT_INJECT':
+                s['other_procs'].append(pid)
+            if subjects_injected == len(subjects):
+                break
+            buf = ''
     f.close()
     os.unlink('rrdump_proc.pipe')
-    for h in handles:
-        if h['handle'].wait() != 0:
-            print('Injector for event {} failed'.format(h['event']))
+    for s in subjects:
+        ret = s['handle'].wait()
+        for i in s['other_procs']:
+            try:
+                os.kill(int(i), signal.SIGKILL)
+            except OSError:
+                pass
+        if ret != 0:
+            print('Injector for event:rec_pid {}:{} failed'
+                  .format(s['event'], s['rec_pid']))
