06121bd0712e0c0b75ec037e3864ed89aec523a9
Preston Moore <prestonkmoore@gmail.com>
2018-06-17 22:26:36 -0700

    Add tests for apply_mmap_backing_files

--- test_inject.py
+++ test_inject.py
@@ -8,6 +8,7 @@ import mock
 
 
 from inject import exit_with_status
+from inject import apply_mmap_backing_files
 
 # pylint: disable=no-self-use
 
@@ -49,3 +50,33 @@ class ExitWithStatusTestCase(unittest.TestCase):
         mock__kill_parent_process.assert_called_with(pid)
         mock_exit.assert_called_with(exit_status)
         mock_print_exc.assert_called()
+
+
+class ApplyMmapBackingFilesTestCase(unittest.TestCase):
+    """Test apply_mmap_backing_files helper function
+    """
+
+    @mock.patch('inject.syscallreplay', spec=['injected_state'])
+    @mock.patch('inject.parse_backing_files')
+    def test_apply_mmap_backing_line(self,
+                                     mock_parse_backing_files,
+                                     mock_syscallreplay):
+        """ Test applying mmap backing file configuration
+        """
+
+        mock_syscallreplay.injected_state = {'config':
+                                             {'mmap_backing_files': '1:/test'}}
+        apply_mmap_backing_files()
+        mock_parse_backing_files.assert_called_with('1:/test')
+
+    @mock.patch('inject.syscallreplay', spec=['injected_state'])
+    @mock.patch('inject.parse_backing_files')
+    def test_apply_mmap_backing_no_line(self,
+                                        mock_parse_backing_files,
+                                        mock_syscallreplay):
+        """ Test not applying backing files when they are not present
+        """
+
+        mock_syscallreplay.injected_state = {'config': {}}
+        apply_mmap_backing_files()
+        mock_parse_backing_files.assert_not_called()
