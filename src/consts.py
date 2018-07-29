7c42187289fa778ad0da712ac45fc923894bc403
ex0dus-0x <ex0dus@codemuch.tech>
2018-07-29 16:00:00 -0400

    implemented rrinit module - introduce cpuid library, consts, and rrinit

---
+++ src/consts.py
@@ -0,0 +1,8 @@
+import os
+
+NAME = "rrapper"
+DEFAULT_CONFIG_PATH = str(os.path.dirname(os.path.expanduser("~") + "/.crashsim/")) + "/"
+RR_TEST_CONFIG_PATH = str(os.path.dirname(os.path.expanduser("~") + "/.local/share/rr/")) + "/"
+
+STRACE_DEFAULT = "strace_out.strace"
+
