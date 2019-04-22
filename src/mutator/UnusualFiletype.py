e2b735755601229874a30fbf5b81c71815f846c2
Preston Moore <prestonkmoore@gmail.com>
2019-04-22 23:51:09 +0100

    Support data parameter, special case for S_IFDIR read calls

--- src/mutator/UnusualFiletype.py
+++ src/mutator/UnusualFiletype.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 from posix_omni_parser import Trace
 from mutator import GenericMutator
 import sys
@@ -24,18 +26,45 @@ class UnusualFiletypeMutator(GenericMutator):
     line = lines[0]
     # TODO: we only support replacing S_IFREG right now
     string_lines[line] = string_lines[line].replace('S_IFREG', self.filetype)
+
+    if self.filetype == 'S_IFDIR':
+        spans = self.find_open_spans_for_file(syscalls, self.name)
+        for span in spans:
+            print(span)
+            fd = syscalls[span[0]].ret[0]
+            reads_in_span = self.find_syscall_between_indexes(syscalls,
+                                                              span[0],
+                                                              span[1],
+                                                              self._match_read,
+                                                              {'file_descriptor': fd})
+            for read in reads_in_span:
+                print(read)
+                string_lines[read] = self._make_read_fail_EISDIR(string_lines[read])
+
+
     with open(trace, 'w') as f:
       for l in string_lines:
         f.write(l)
 
-  def _match_fstat(self, syscall):
+
+  def _make_read_fail_EISDIR(self, line):
+      equals_index = line.rfind('=')
+      return line[:equals_index] + ' = -1 EISDIR\n'
+
+  def _match_read(self, syscall, data=None):
+      if syscall.name.startswith('read'):
+          if syscall.args[0].value == data['file_descriptor']:
+              return True
+
+
+  def _match_fstat(self, syscall, data=None):
       if syscall.name.startswith('fstat'):
         if self.file_descriptor:
           if self.file_descriptor != syscall.args[0].value:
               return False
         return True
 
-  def _match_stat_or_lstat(self, syscall):
+  def _match_stat_or_lstat(self, syscall, data=None):
       # fstat takes a file descriptor
       # stat and lstat take a name rather than a file descriptor
       if syscall.name.startswith('stat') or syscall.name.startswith('lstat'):
@@ -44,5 +73,5 @@ class UnusualFiletypeMutator(GenericMutator):
             return False
         return True
 
-  def _match_statlike(self, syscall):
+  def _match_statlike(self, syscall, data=None):
     return self._match_stat_or_lstat(syscall) or self._match_fstat(syscall)
--- test_unusual_filetype_mutator.py
+++ test_unusual_filetype_mutator.py
@@ -2,6 +2,8 @@
 """ Test the UnusualFiletypeMutator
 """
 
+from __future__ import print_function
+
 from src.mutator.UnusualFiletype import UnusualFiletypeMutator
 
 import unittest
@@ -11,7 +13,7 @@ import os
 from bunch import Bunch
 
 
-class TestRequiredMethods(unittest.TestCase):
+class TestNonIFDIRMutations(unittest.TestCase):
   def test_init_both_name_and_file_descriptor(self):
     self.assertRaises(UnusualFiletypeMutator, name='test.txt',
                                               file_descriptor=3)
@@ -60,3 +62,30 @@ class TestRequiredMethods(unittest.TestCase):
       line = data[0]
       self.assertTrue('S_IFCHR' in line)
     os.unlink(tmp.name)
+
+
+class TestIFDIRMutations(unittest.TestCase):
+  def test_mutate_stat_call(self):
+    calls_str = r'''118798 stat("test.txt", {st_dev=makedev(8, 1), st_ino=1182893, st_mode=S_IFREG|0664, st_nlink=1, st_uid=1000, st_gid=1000, st_blksize=4096, st_blocks=8, st_size=5, st_atime=1555971967 /* 2019-04-22T23:26:07.806253267+0100 */, st_atime_nsec=806253267, st_mtime=1555971967 /* 2019-04-22T23:26:07.806253267+0100 */, st_mtime_nsec=806253267, st_ctime=1555971967 /* 2019-04-22T23:26:07.806253267+0100 */, st_ctime_nsec=806253267}) = 0
+118798 openat(AT_FDCWD, "test.txt", O_RDONLY) = 3
+118798 fadvise64(3, 0, 0, POSIX_FADV_SEQUENTIAL) = 0
+118798 read(3, "test\n", 16384)         = 5
+118798 openat(AT_FDCWD, "/usr/lib/x86_64-linux-gnu/gconv/gconv-modules.cache", O_RDONLY) = 4
+118798 mmap(NULL, 26376, PROT_READ, MAP_SHARED, 4, 0) = 0x7ffff7ff0000
+118798 close(4)                         = 0
+118798 read(3, "", 16384)               = 0
+118798 write(1, "1 1 5 test.txt\n", 15) = 15
+118798 close(3)                         = 0
+118798 close(1)                         = 0
+118798 close(2)                         = 0
+118798 exit_group(0)                    = ?
+'''
+    tmp = tempfile.NamedTemporaryFile(delete=False)
+    tmp.write(calls_str)
+    tmp.close()
+    mutator = UnusualFiletypeMutator(filetype='S_IFDIR', name='test.txt')
+    mutator.mutate_trace(tmp.name)
+    with open(tmp.name) as f:
+      lines = f.readlines()
+      self.assertTrue(' = -1 EISDIR\n' in lines[3])
+      self.assertTrue(' = -1 EISDIR\n' in lines[7])
