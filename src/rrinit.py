e60b9676164e6e3f4c3eddf504f8148aaf5e680c
Preston Moore <prestonkmoore@gmail.com>
2019-04-25 21:59:48 +0100

    Small cleanup

--- src/rrinit.py
+++ src/rrinit.py
@@ -27,6 +27,8 @@
 
 """
 
+from __future__ import print_function
+
 import os
 import sys
 import errno
@@ -58,46 +60,44 @@ def main():
   logging.basicConfig(level=args.loglevel)
 
   # check CPUID environment
-  logging.debug("Checking CPU architecture compatibility")
+  logging.debug('Checking CPU architecture compatibility')
   if cpuid.cpuid_check() != 0:
     exit(1)
 
   # checking if disabled ASLR
-  logging.debug("Checking if ASLR is disabled")
+  logging.debug('Checking if ASLR is disabled')
   with open('/proc/sys/kernel/randomize_va_space', 'r') as proc:
-    if proc.read() == '1':
-      print "ASLR should be disabled. \
-      Set /proc/sys/kernel/randomize_va_space = 0"
+    if proc.read() != '0':
+      print('ASLR should be disabled. Set /proc/sys/kernel/randomize_va_space = 0')
       sys.exit(1)
 
   # checking if disabled ptrace on processes
-  logging.debug("Checking if ptrace on processes is enabled")
+  logging.debug('Checking if ptrace on processes is enabled')
   with open('/proc/sys/kernel/yama/ptrace_scope', 'r') as proc:
-    if proc.read() == '1':
-      print "ptrace on processes should be disabled \
-      Set /proc/sys/kernel/yama/ptrace_scope = 0"
+    if proc.read() != '0':
+      print('ptrace on processes should be allowed. Set /proc/sys/kernel/yama/ptrace_scope = 0')
+
+
       sys.exit(1)
 
   # check to see if rr is a valid shell-level command.
   # error status is nonzero
-  logging.debug("Checking if `rr` is a valid shell command")
+  logging.debug('Checking if `rr` is a valid shell command')
   try:
     with open(os.devnull, 'w') as fnull:
-      subprocess.check_call(['rr', 'help'],
-                           stdout=fnull,
-													 stderr=subprocess.STDOUT)
+      subprocess.check_call(['rr', 'help'], stdout=fnull, stderr=subprocess.STDOUT)
   # in the case that the command failed to execute for internal reasons
   except subprocess.CalledProcessError:
-    print 'rr was found by "rr help" exited with an error.'
+    print('rr was found by "rr help" exited with an error.')
     sys.exit(1)
   # in the case that the command is not valid at all
   except OSError:
-    print 'rr command is not found. Make sure it is installed and described within your $PATH'
+    print('rr command is not found. Make sure it is installed and described within your $PATH')
     sys.exit(1)
 
   # create ~/.crashsim/
   if not os.path.exists(consts.DEFAULT_CONFIG_PATH):
-    logging.debug("Creating configuration path")
+    logging.debug('Creating configuration path')
     try:
       os.makedirs(consts.DEFAULT_CONFIG_PATH)
     except OSError as err:
@@ -105,16 +105,16 @@ def main():
         raise
 
   # check for existence of rrdump pipe
-  logging.debug("Checking if pipe already exists")
-  if os.path.exists(consts.DEFAULT_CONFIG_PATH + "rrdump.pipe"):
-    os.unlink(consts.DEFAULT_CONFIG_PATH + "rrdump.pipe")
+  logging.debug('Checking if pipe already exists')
+  if os.path.exists(consts.DEFAULT_CONFIG_PATH + 'rrdump.pipe'):
+    os.unlink(consts.DEFAULT_CONFIG_PATH + 'rrdump.pipe')
 
   # generate pickle
-  if not os.path.exists(consts.DEFAULT_CONFIG_PATH + "syscall_definitions.pickle"):
-    print "syscall_definitions.pickle does NOT exist. Regenerating."
+  if not os.path.exists(consts.DEFAULT_CONFIG_PATH + 'syscall_definitions.pickle'):
+    print('syscall_definitions.pickle does NOT exist. Regenerating.')
     parse_syscall_definitions.generate_pickle()
-  print "\n=========================================================="
-  print "Sucessfully initialized CrashSimulator environment!"
+  print('\n==========================================================')
+  print('Sucessfully initialized CrashSimulator environment!')
   sys.exit(0)
 
 
