f166bf6bff88d55231047ffbee0fe99e7d4e5680
Preston Moore <prestonkmoore@gmail.com>
2019-05-31 12:47:26 -0700

    Fix tabbing

--- src/mutator/CrossdiskRename.py
+++ src/mutator/CrossdiskRename.py
@@ -25,14 +25,14 @@ class CrossdiskRenameMutator:
         f.write(l)
 
   def identify_lines(self,trace):
-  	lines = []
-	with open(trace, 'r') as f:
-		string_lines = f.readlines()
-	syscalls = Trace.Trace(trace, DEFAULT_CONFIG_PATH + 'syscall_definitions.pickle').syscalls
-	for k, v in enumerate(syscalls):
-		if v.name == 'rename':
-			if self.name:
-			  if v.args[0].value != self.name:
-			    continue
-			lines.append(k)
-	return lines
+    lines = []
+    with open(trace, 'r') as f:
+      string_lines = f.readlines()
+    syscalls = Trace.Trace(trace, DEFAULT_CONFIG_PATH + 'syscall_definitions.pickle').syscalls
+    for k, v in enumerate(syscalls):
+      if v.name == 'rename':
+        if self.name:
+          if v.args[0].value != self.name:
+            continue
+        lines.append(k)
+    return lines
