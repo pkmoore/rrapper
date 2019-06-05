f239101832c0c516e7ec4d55eecb869b1d8d05c6
Junjie Ge <jg5324@nyu.edu>
2019-06-05 13:51:34 -0400

    removed unused file open

--- src/mutator/FutureTime.py
+++ src/mutator/FutureTime.py
@@ -22,8 +22,6 @@ class FutureTimeMutator:
 
   def identify_lines(self,trace):
     lines = []
-    with open(trace, 'r') as f:
-      string_lines = f.readlines()
     syscalls = Trace.Trace(trace, DEFAULT_CONFIG_PATH + 'syscall_definitions.pickle').syscalls
     for k, v in enumerate(syscalls):
       if v.name == 'time':
