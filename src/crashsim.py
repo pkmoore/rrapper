d0071bb585bd227e31cbf101a4bb347082c9fa4a
Junjie Ge <jg5324@nyu.edu>
2019-06-06 14:58:29 -0400

    added logging, removed print

--- src/crashsim.py
+++ src/crashsim.py
@@ -62,24 +62,24 @@ def main():
   proc_create = subprocess.Popen(["rrtest", "create", "--name", args.cmd, "--command", args.command])
   proc_create.wait()
 
+  logging.debug("Checking if rrtest create is successfull")
   if proc_create.returncode != 0:
-    print("Test creation failed")
     sys.exit(1)
 
   # configuring the test
   proc_configure = subprocess.Popen(["rrtest", "configure", "--name", args.cmd, "--mutator", args.mutator]) 
   proc_configure.wait()
 
+  logging.debug("Checking if rrtest configure is successfull")
   if proc_configure.returncode != 0:
-    print("Test configuration failed")
     sys.exit(1)
   
   # replay the test
   proc_replay = subprocess.Popen(["rreplay", args.cmd])
   proc_replay.wait()
 
+  logging.debug("Checking if rreplay is successfull")
   if proc_replay.returncode != 0:
-    print("Replay failed")
     sys.exit(1)
 
 if __name__ == "__main__":
