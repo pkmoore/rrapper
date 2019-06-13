875b96ea29be5981ad8533a42d0fff885d42e5ab
Sean Chai <seancyx@hotmail.com>
2019-06-13 15:43:16 -0400

    Fix test_inject.py errors and add new tests on function handle_syscall.

--- test_inject.py
+++ test_inject.py
@@ -8,6 +8,8 @@ import mock
 from bunch import Bunch
 
 from syscallreplay.util import ReplayDeltaError
+from syscallreplay import util
+import syscallreplay
 
 from inject import exit_with_status
 from inject import apply_mmap_backing_files
@@ -15,6 +17,7 @@ from inject import apply_open_fds
 from inject import consume_configuration
 from inject import parse_backing_files
 from inject import debug_handle_syscall
+from inject import handle_syscall
 
 # pylint: disable=no-self-use
 
@@ -23,39 +26,51 @@ class TestExitWithStatusTestCase(unittest.TestCase):
     """Test exit_with_status helper function
     """
 
+    @mock.patch('re.findall')
     @mock.patch('inject._kill_parent_process')
     @mock.patch('traceback.print_exc')
     @mock.patch('sys.exit')
     def test_exit_with_zero_status(self,
                                    mock_exit,
                                    mock_print_exc,
-                                   mock__kill_parent_process):
+                                   mock__kill_parent_process,
+                                   mock_re_findall):
         """ Test correctly exiting with zero status
         """
 
         pid = 555
         exit_status = 0
-        exit_with_status(pid, exit_status)
+        mutator = 'src.mutator.ReverseTime.ReverseTimeMutator'
+        event = 100
+        index = 2
+        exit_with_status(pid, exit_status, mutator, event, index)
         mock__kill_parent_process.assert_called_with(pid)
         mock_exit.assert_called_with(exit_status)
         mock_print_exc.assert_not_called()
+        mock_re_findall.assert_called_with(r'src.mutator.\w+.(\w+)', mutator)
 
+    @mock.patch('re.findall')
     @mock.patch('inject._kill_parent_process')
     @mock.patch('traceback.print_exc')
     @mock.patch('sys.exit')
     def test_exit_with_non_zero_status(self,
                                        mock_exit,
                                        mock_print_exc,
-                                       mock__kill_parent_process):
+                                       mock__kill_parent_process,
+                                       mock_re_findall):
         """ Test correctly exiting with non-zero status
         """
 
         pid = 555
         exit_status = -1
-        exit_with_status(pid, exit_status)
+        mutator = 'src.mutator.ReverseTime.ReverseTimeMutator'
+        event = 100
+        index = 2
+        exit_with_status(pid, exit_status, mutator, event, index)
         mock__kill_parent_process.assert_called_with(pid)
         mock_exit.assert_called_with(exit_status)
         mock_print_exc.assert_called()
+        mock_re_findall.assert_called_with(r'src.mutator.\w+.(\w+)', mutator)
 
 
 class TestApplyMmapBackingFilesTestCase(unittest.TestCase):
@@ -164,3 +179,32 @@ class TestDebugHandleSyscall(unittest.TestCase):
                           syscall_object,
                           entering)
         mock_printer.assert_called_with(pid, syscall_id, syscall_object)
+
+
+class TestHandleSyscall(unittest.TestCase):
+    """ Test handle_syscall
+    """
+
+    @mock.patch('inject.handle_socketcall')
+    @mock.patch('inject.util.validate_syscall')
+    def test_handle_syscall_with_syscallid_102(self, mock_validate_syscall, mock_handle_socketcall):
+        pid = 555
+        syscall_id = 102
+        syscall_object = Bunch()
+        entering = True
+        handle_syscall(pid, syscall_id, syscall_object, entering)
+        mock_handle_socketcall.assert_called_with(syscall_id, syscall_object, entering, pid)
+        mock_validate_syscall.assert_not_called()
+
+
+    @mock.patch('inject.handle_socketcall')
+    @mock.patch('inject.util.validate_syscall')
+    def test_handle_syscall_with_syscallid_not102(self, mock_validate_syscall, mock_handle_socketcall):
+        pid = 555
+        syscall_id = 91
+        syscall_object = Bunch()
+        syscall_object.name = 'open'
+        entering = True
+        handle_syscall(pid, syscall_id, syscall_object, entering)
+        mock_handle_socketcall.assert_not_called()
+        mock_validate_syscall.assert_called_with(syscall_id, syscall_object)
