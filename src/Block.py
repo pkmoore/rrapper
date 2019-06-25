5b2c02e82693a1e17d47592e354546addd0fc67b
Junjie Ge <jg5324@nyu.edu>
2019-06-25 13:44:31 -0400

    added comment in Block.py

--- src/Block.py
+++ src/Block.py
@@ -1,3 +1,20 @@
+"""
+<Program Name>
+  Block
+
+<Started>
+  June 2019
+
+<Author>
+  Junjie Ge
+
+<Purpose>
+  Every block is a "window" within all the syscalls, the purpose of each of the
+  block is to divide the syscalls into multiple blocks in case we identify
+  too many syscalls for one program. Breaking it into blocks potentially makes
+  the mutators run faster
+
+"""
 
 from posix_omni_parser.parsers.StraceParser import StraceParser
 
@@ -15,15 +32,21 @@ class Block:
   def _read_file(self, file_handle):
     syscalls_list = []
     trace_lines_list = []
+
+    # define the window to be a constant. The block is smaller if the remaining
+    # lines is less than that constant
     for i in range(1000):
       try:
         trace_line = file_handle.next()
       except StopIteration:
         self.eof = 1
         break
+      
+      # parses the strace line by line
       current_syscall = self.parser.parse_line(trace_line)
       syscalls_list.append(current_syscall)
       trace_lines_list.append(trace_line) 
+
     return [syscalls_list, trace_lines_list]
  
   def get_block(self):
