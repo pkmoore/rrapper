bcb2864b8ef42259c4eed5312d1d456badb02b4f
Preston Moore <prestonkmoore@pm.me>
2019-06-26 14:53:56 -0700

    Add some tests around inject vs don't inject

--- test_rreplay.py
+++ test_rreplay.py
@@ -5,11 +5,13 @@
 
 import unittest
 import mock
+import bunch
 
 
 from rreplay import get_configuration
 from rreplay import execute_rr
 from rreplay import wait_on_handles
+from rreplay import process_messages
 
 # pylint: disable=no-self-use
 
@@ -29,9 +31,10 @@ class TestGetConfiguration(unittest.TestCase):
             get_configuration(config_file)
         except IOError:
             pass
-        
+
         mock_logger.debug.assert_called()
         mock_config_read.assert_called_with(config_file)
+
 
 
 
@@ -57,6 +60,124 @@ class TestExecuteRR(unittest.TestCase):
         # check if proc.out is opened for writing output
         mock_open.assert_called_with('proc.out', 'w')
         mock_popen.assert_called()
+
+
+
+
+
+class TestProcessMessages(unittest.TestCase):
+  """Test process_messages
+  """
+
+  @mock.patch('rreplay.get_message', return_value='{bad}')
+  def test_invalid_json_message(self, mock_get_message):
+    """A badly formatted json message should result in a ValueError
+    """
+
+    subjects = [{'rec_pid': '123', 'event': '123'}]
+    self.assertRaises(ValueError, process_messages, subjects)
+
+
+
+  @mock.patch('rreplay.get_message', return_value='''{
+                                                     "inject": "true",
+                                                     "event": "140",
+                                                     "pid": "120",
+                                                     "rec_pid": "1100",
+                                                     "brks": []
+                                                   }''')
+  @mock.patch('rreplay.logger')
+  @mock.patch('rreplay.json.dump')
+  @mock.patch('rreplay.util.process_is_alive', return_value=True)
+  @mock.patch('rreplay.open')
+  @mock.patch('rreplay.json.load', return_value={"rec_pid": "1849",
+                                                 "trace_file": "/home/preston/.crashsim/mytest32/trace_snip0.strace",
+                                                 "injected_state_file": "140_FutureTimeMutator()_state.json",
+                                                 "mutator": "FutureTimeMutator()",
+                                                 "trace_start": "0",
+                                                 "trace_end": "5",
+                                                 "event": "140",
+                                                 "other_procs": []})
+  @mock.patch('rreplay.subprocess.Popen', return_value=bunch.Bunch)
+  def test_good_json_message(self,
+                             mock_popen,
+                             mock_json_load,
+                             mock_open,
+                             mock_process_is_alive,
+                             mock_json_dump,
+                             mock_logger,
+                             mock_get_message):
+    """A well formatted json message with inject == true should be handled
+    """
+
+    subjects = [{'mutator': 'NullMutator(70)',
+                 'rec_pid': '1100',
+                 'event': '140',
+                 'injected_state_file': '140_NullMutator(70)_state.json'}]
+    process_messages(subjects)
+    mock_process_is_alive.assert_called_with('120')
+    # make sure we open the eventwise config file
+    mock_open.assert_has_calls([mock.call('140_NullMutator(70)_state.json', 'r')])
+    mock_json_load.assert_called()
+    # make sure we dump out the pid unique config file after we have
+    # generated it
+    mock_json_dump.assert_called()
+    # make sure we spin off a subprocess with the correct parameters
+    # based on the configured state
+    mock_popen.assert_called_with(['inject',
+                                   '--verbosity=40',
+                                   '120_140_NullMutator(70)_state.json'])
+
+
+  @mock.patch('rreplay.get_message', return_value='''{
+                                                     "inject": "false",
+                                                     "event": "140",
+                                                     "pid": "120",
+                                                     "rec_pid": "1100",
+                                                     "brks": []
+                                                   }''')
+  @mock.patch('rreplay.logger')
+  @mock.patch('rreplay.json.dump')
+  @mock.patch('rreplay.util.process_is_alive', return_value=True)
+  @mock.patch('rreplay.open')
+  @mock.patch('rreplay.json.load', return_value={"rec_pid": "1849",
+                                                 "trace_file": "/home/preston/.crashsim/mytest32/trace_snip0.strace",
+                                                 "injected_state_file": "140_FutureTimeMutator()_state.json",
+                                                 "mutator": "FutureTimeMutator()",
+                                                 "trace_start": "0",
+                                                 "trace_end": "5",
+                                                 "event": "140",
+                                                 "other_procs": []})
+  @mock.patch('rreplay.subprocess.Popen', return_value=bunch.Bunch)
+  def test_good_json_message(self,
+                             mock_popen,
+                             mock_json_load,
+                             mock_open,
+                             mock_process_is_alive,
+                             mock_json_dump,
+                             mock_logger,
+                             mock_get_message):
+    """A well formatted json message with inject == false should not
+       spawn a subprocess
+    """
+
+    subjects = [{'mutator': 'NullMutator(70)',
+                 'rec_pid': '1100',
+                 'event': '140',
+                 'injected_state_file': '140_NullMutator(70)_state.json',
+                 'other_procs': []}]
+    process_messages(subjects)
+    mock_process_is_alive.assert_called_with('120')
+    # make sure we open the eventwise config file
+    mock_open.not_called()
+    mock_json_load.assert_not_called()
+    # make sure we dump out the pid unique config file after we have
+    # generated it
+    mock_json_dump.assert_not_called()
+    # make sure we spin off a subprocess with the correct parameters
+    # based on the configured state
+    self.assertEqual(subjects[0]['other_procs'], ['120'])
+
 
 
 
