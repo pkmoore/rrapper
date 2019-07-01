c007a7673d6a7a68f0f00365f158d7e1c164ea10
Sean Chai <seancyx@hotmail.com>
2019-07-01 14:59:51 -0400

    Add unit tests for mutate_syscall function

--- src/mutator/test_UnusualFiletype.py
+++ src/mutator/test_UnusualFiletype.py
@@ -90,3 +90,44 @@ class TestIdentify(unittest.TestCase):
     mut = UnusualFiletypeMutator()
     lines = mut.identify_lines(syscalls)
     self.assertEqual(len(lines), 3)
+
+  def test_mutate_syscall_replace_SIFREG_with_SIF(self):
+    ''' UnusualFiletypeMutator should mutate S_IFREG to S_IF
+    '''
+    trace_data = '28725 lstat64(".data.txt.TziqM5", {st_dev=makedev(8, 1), st_ino=50795, st_mode=S_IFREG|0600, st_nlink=1, st_uid=1000, st_gid=1000, st_blksize=4096, st_blocks=8, st_size=13, st_atime=2018/05/06-16:29:03.502410913, st_mtime=2018/05/06-16:29:03.502410913, st_ctime=2018/05/06-16:29:03.502410913}) = 0'
+    trace_file = tempfile.NamedTemporaryFile()
+    trace_file.write(trace_data)
+    trace_file.flush()
+    syscalls = Trace.Trace(trace_file.name, DEFAULT_CONFIG_PATH + 'syscall_definitions.pickle').syscalls
+    trace_file.close()
+    mut = UnusualFiletypeMutator('S_IF')
+    mut.mutate_syscalls(syscalls)
+    test_trace_data = '28725 lstat64(".data.txt.TziqM5", {st_dev=makedev(8, 1), st_ino=50795, st_mode=S_IF|0600, st_nlink=1, st_uid=1000, st_gid=1000, st_blksize=4096, st_blocks=8, st_size=13, st_atime=2018/05/06-16:29:03.502410913, st_mtime=2018/05/06-16:29:03.502410913, st_ctime=2018/05/06-16:29:03.502410913}) = 0'
+    test_trace_file = tempfile.NamedTemporaryFile()
+    test_trace_file.write(test_trace_data)
+    test_trace_file.flush()
+    test_syscalls = Trace.Trace(test_trace_file.name, DEFAULT_CONFIG_PATH + 'syscall_definitions.pickle').syscalls
+    test_trace_file.close()
+    for i in range(len(syscalls[0].args)):
+      self.assertEqual(syscalls[0].args[i].value, test_syscalls[0].args[i].value)
+
+  def test_mutate_syscall_replace_SIF_with_SIFREG(self):
+    ''' UnusualFiletypeMutator should mutate S_IF to S_IFREG
+    '''
+    trace_data = '28725 lstat64(".data.txt.TziqM5", {st_dev=makedev(8, 1), st_ino=50795, st_mode=S_IF|0600, st_nlink=1, st_uid=1000, st_gid=1000, st_blksize=4096, st_blocks=8, st_size=13, st_atime=2018/05/06-16:29:03.502410913, st_mtime=2018/05/06-16:29:03.502410913, st_ctime=2018/05/06-16:29:03.502410913}) = 0'
+    trace_file = tempfile.NamedTemporaryFile()
+    trace_file.write(trace_data)
+    trace_file.flush()
+    syscalls = Trace.Trace(trace_file.name, DEFAULT_CONFIG_PATH + 'syscall_definitions.pickle').syscalls
+    trace_file.close()
+    mut = UnusualFiletypeMutator('S_IFREG')
+    mut.mutate_syscalls(syscalls)
+    test_trace_data = '28725 lstat64(".data.txt.TziqM5", {st_dev=makedev(8, 1), st_ino=50795, st_mode=S_IFREG|0600, st_nlink=1, st_uid=1000, st_gid=1000, st_blksize=4096, st_blocks=8, st_size=13, st_atime=2018/05/06-16:29:03.502410913, st_mtime=2018/05/06-16:29:03.502410913, st_ctime=2018/05/06-16:29:03.502410913}) = 0'
+    test_trace_file = tempfile.NamedTemporaryFile()
+    test_trace_file.write(test_trace_data)
+    test_trace_file.flush()
+    test_syscalls = Trace.Trace(test_trace_file.name, DEFAULT_CONFIG_PATH + 'syscall_definitions.pickle').syscalls
+    test_trace_file.close()
+    for i in range(len(syscalls[0].args)):
+      self.assertEqual(syscalls[0].args[i].value, test_syscalls[0].args[i].value)
+
